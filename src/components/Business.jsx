eafe24ba2d28d42430a73374b5359e7c39d2e93c
Amirhossein2292 <amirsalehi248@yahoo.com>
2022-09-17 17:10:56 +0430

    Add Header and features

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import { features } from '../constants';
+import styles, { layout } from '../style'
+import Button from './Button';
+
+const FeatureCard = ( {icon, title, content, index}) => (
+    <div>
+        <div>
+            <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+        </div>
+    </div>
+)
+
 
 const Business = () => {
     return (
-        <div>
-            Business
-        </div>
+        <section id="features" className={layout.section}>
+            <div className={layout.sectionInfo}>
+                <h2 className={styles.heading2}>You do the business, <br className="sm:block hidden" /></h2>
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit quae odio soluta atque libero hic cumque dolor labore et ut!</p>
+
+                <Button styles="mt-20" />
+
+                <div className={`${layout.sectionImg} flex-col`}>
+                    {features.map((feature, index) => (
+                        <FeatureCard key={feature.id} {...feature} index={index} />
+                    ))}
+                </div>
+            </div>
+        </section>
     );
 };
 
